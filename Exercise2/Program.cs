a712d699cb469f96a796a637dfd3c9ce3d1876a0
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-04 16:21:44 +0700

    add function

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,11 +1,30 @@
-﻿
-    class Program
+﻿using System;
+namespace Bubble_sort
 {
-    //Array to be searched
-    int[] arr = new int[68];
-    //Number of the elements in the array
-    int j;
-    int n;
-}
+    class Program
+    {
 
-   
+        //Deklarasi array int dengan ukuran 68
+        private int[] a = new int[68];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array 
+        private int n;
+
+        //Fungsi /method untuk menerima masukan 
+
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimppan pada array 
+            while (true)
+            {
+                Console.WriteLine("Masukkan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 92)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 92 elemen.\n");
+            }
+        }
+    }
+}
